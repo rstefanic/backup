c3a605bb5810797d9035d7b26fe455c490ab6c5e
Robert Stefanic <rstefanic72@gmail.com>
2017-04-30 22:06:13 +0000

    added progress bar to file copying

--- backup.py
+++ backup.py
@@ -8,8 +8,9 @@ def main():
     errorcheck_source(source_path)
 
     total_size = get_total_size(source_path)
-    print(progress_bar(5029, total_size))
-    #copy_directory(source_path, destination_path)
+    print("total src size: %s " % total_size)
+    copy_directory(source_path, destination_path, total_size)
+    
 
 
 def progress_bar(current_progress, total):
@@ -25,19 +26,31 @@ def progress_bar(current_progress, total):
     bar = reduce((lambda x, y: x + y),
              map((lambda x: determine_bartype(x)),
              range(1, 30)))
-    return ("Copying Files |%s| %.f%% Complete " %
+    return ("Copying Files [%s] %.f%% Complete " %
             (bar, percent))
 
-def copy_directory(source, destination):
+def copy_directory(source, destination, total_size):
 
-    for file in os.listdir(source):
-        current_file = os.path.join(source, file)
-        if os.path.isdir(current_file):
-            os.makedirs(os.path.join(destination, file))
-            copy_directory(current_file, os.path.join(destination, file))
-        else:
-            shutil.copy2(current_file, destination)
-            
+    def copy_files(src, dest, bytes_copied):
+        
+        for file in os.listdir(src):
+            cur_file = os.path.join(src, file)
+            if os.path.isfile(cur_file):
+                shutil.copy2(cur_file, dest)
+                bytes_copied += os.path.getsize(cur_file)
+                print(progress_bar(bytes_copied, total_size))
+                continue
+            else:
+                os.makedirs(os.path.join(dest, file))
+                copy_files(cur_file,
+                           os.path.join(dest, file),
+                           bytes_copied)
+                bytes_copied += get_total_size(cur_file)
+
+        print(progress_bar(bytes_copied, total_size))
+        
+    copy_files(source, destination, 0)
+
 
 def get_total_size(path):
 
@@ -46,9 +59,12 @@ def get_total_size(path):
             if os.listdir(os.path.join(path, current_file)):
                 return get_total_size(os.path.join(path, current_file))
         return os.path.getsize(os.path.join(path, current_file))
-    
-    return reduce((lambda x, y: x + y),
-              map((lambda x: get_size(x)), os.listdir(path)))
+
+    if os.listdir(path):
+        return reduce((lambda x, y: x + y),
+                  map((lambda x: get_size(x)), os.listdir(path)))
+    else:
+        return 0
 
 def assign_path(argv_position):
     return os.path.abspath(sys.argv[argv_position])
